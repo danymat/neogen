7e97b9a71ea29cad9b5765a57d1ca074c877ec4a
Barak Michener <me@barakmich.com>
2022-03-03 01:43:26 -0800

    Improve Godoc support (#75)
    * Improve Godoc support
    
    Godoc recommends, as per https://go.dev/blog/godoc , that documentation
    comments start with the name of the thing being documented.
    
    This adds that, as well as better support for structs and interfaces.
    
    * ref(go) Use `as` keyword in configuration trees
    
    * chore: Bump neogen version
    
    Co-authored-by: danymat <d.danymat@gmail.com>

--- lua/neogen/configurations/go.lua
+++ lua/neogen/configurations/go.lua
@@ -1,17 +1,46 @@
 local template = require("neogen.template")
+local extractors = require("neogen.utilities.extractors")
+local nodes_utils = require("neogen.utilities.nodes")
 
 return {
     parent = {
-        func = { "function_declaration" },
-        type = { "package_clause", "const_declaration", "var_declaration" },
+        func = { "function_declaration", "method_declaration" },
+        type = { "package_clause", "const_declaration", "var_declaration", "type_declaration" },
     },
 
     data = {
         func = {
             ["function_declaration"] = {
                 ["0"] = {
-                    extract = function()
-                        return {}
+                    extract = function(node)
+                        local tree = {
+                            {
+                                retrieve = "first",
+                                node_type = "identifier",
+                                extract = "true",
+                                as = "func_name",
+                            },
+                        }
+                        local nodes = nodes_utils:matching_nodes_from(node, tree)
+                        local res = extractors:extract_from_matched(nodes)
+                        return res
+                    end,
+                },
+            },
+            ["method_declaration"] = {
+                ["0"] = {
+                    extract = function(node)
+                        local tree = {
+                            {
+                                retrieve = "first",
+                                node_type = "field_identifier",
+                                extract = "true",
+                                as = "func_name",
+                            },
+                        }
+                        local nodes = nodes_utils:matching_nodes_from(node, tree)
+                        local res = extractors:extract_from_matched(nodes)
+                        return res
                     end,
                 },
             },
@@ -21,6 +50,29 @@ return {
                 ["0"] = {
                     extract = function()
                         return {}
+                    end,
+                },
+            },
+            ["type_declaration"] = {
+                ["0"] = {
+                    extract = function(node)
+                        local tree = {
+                            {
+                                retrieve = "all",
+                                node_type = "type_spec",
+                                subtree = {
+                                    {
+                                        retrieve = "first",
+                                        node_type = "type_identifier",
+                                        extract = true,
+                                        as = "type_name",
+                                    },
+                                },
+                            },
+                        }
+                        local nodes = nodes_utils:matching_nodes_from(node, tree)
+                        local res = extractors:extract_from_matched(nodes)
+                        return res
                     end,
                 },
             },
