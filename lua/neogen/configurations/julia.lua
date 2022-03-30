aa9fb0ef0506ffea8704e8f177a85ebac6ca54de
josephsdavid <josephsd@smu.edu>
2022-03-30 00:43:45 -0500

    types on one line please?

--- lua/neogen/configurations/julia.lua
+++ lua/neogen/configurations/julia.lua
@@ -84,8 +84,8 @@ return {
                             results["typed_parameters"] = {}
                             for _, n in pairs(nodes["typed_parameter"]) do
                                 local type_subtree = {
-                                    { retrieve = "all", node_type = "identifier", extract = true, as = i.Parameter },
-                                    { retrieve = "all", node_type = "type", extract = true, as = i.Type },
+                                    { retrieve = "first", node_type = "identifier", extract = true, as = i.Parameter },
+                                    { retrieve = "last", node_type = "identifier", extract = true, as = i.Type },
                                 }
                                 local typed_parameters = nodes_utils:matching_nodes_from(n, type_subtree)
                                 typed_parameters = extractors:extract_from_matched(typed_parameters)
