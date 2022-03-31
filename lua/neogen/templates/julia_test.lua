9746931e15879cfdebe73b2195cc38f49566372f
josephsdavid <josephsd@smu.edu>
2022-03-30 22:59:41 -0500

    so much fun to be had

--- lua/neogen/templates/julia_test.lua
+++ lua/neogen/templates/julia_test.lua
@@ -1,14 +1,18 @@
 local i = require("neogen.types.template").item
 
 return {
+    { nil, '""" $1 """', { no_results = true, type = { "func" } } },
+    { nil, '"""$1' },
     {
         { i.Parameter, i.Type },
-        "# %s : %s $1",
+        "%s::%s - $1",
         { required = "typed_parameters", type = { "func" } },
     },
     {
-        { i.Parameter, },
-        "# %s : $1",
-        { required = "parameters", type = { "func" } },
+         i.Parameter,
+        "%s - $1",
+        { required = "identifier", type = { "func" } },
     },
+
+    { nil, '"""' },
 }
