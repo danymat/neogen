cd7dcd7056d391dcaf1fabd0efa76442b76e2d51
josephsdavid <josephsd@smu.edu>
2022-03-31 10:51:43 -0500

    formatting from lighthouse.jl

--- lua/neogen/templates/julia_test.lua
+++ lua/neogen/templates/julia_test.lua
@@ -3,15 +3,17 @@ local i = require("neogen.types.template").item
 return {
     { nil, '""" $1 """', { no_results = true, type = { "func" } } },
     { nil, '"""' },
-    { nil, '$1' },
+    { nil, 'Overview:' },
+    { nil, '\t$1' },
+    { nil, 'Where:' },
     {
         { i.Parameter, i.Type },
-        "\t%s::%s: $1",
+        "\t%s::%s is $1",
         { required = "typed_parameters", type = { "func" } },
     },
     {
          i.Parameter,
-        "\t%s: $1",
+        "\t%s is $1",
         { required = "parameters", type = { "func" } },
     },
 
