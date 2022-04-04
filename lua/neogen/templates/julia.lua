0fc8d7e9dae626c0ef8ac0f71ca5f6c1bde91ea0
josephsdavid <josephsd@smu.edu>
2022-04-04 10:24:08 -0500

    struct docs?

--- lua/neogen/templates/julia.lua
+++ lua/neogen/templates/julia.lua
@@ -2,6 +2,7 @@ local i = require("neogen.types.template").item
 
 return {
     { nil, '""" $1 """', { no_results = true, type = { "func" } } },
+    { nil, '""" $1 """', { no_results = true, type = { "struct" } } },
     { nil, '"""' },
     { nil, 'Overview:' },
     { nil, '' },
@@ -9,9 +10,19 @@ return {
     {nil, ''},
     { nil, 'Where...' },
     {
+         i.Parameter,
+        "\t%s is $1",
+        { required = "parameters", type = { "struct" } },
+    },
+    {
         { i.Parameter, i.Type },
         "\t%s::%s is $1",
         { required = "typed_parameters", type = { "func" } },
+    },
+    {
+        { i.Parameter, i.Type },
+        "\t%s::%s is $1",
+        { required = "typed_parameters", type = { "struct" } },
     },
     {
          i.Parameter,
