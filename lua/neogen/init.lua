b1e56c5263f3155f6368a0a173b06d84257c2c7d
danymat <d.danymat@gmail.com>
2022-10-20 13:32:37 +0200

    fix(ts) Do not offset function annotations (#111)

--- lua/neogen/init.lua
+++ lua/neogen/init.lua
@@ -285,7 +285,7 @@ end
 ---     with multiple annotation conventions.
 ---@tag neogen-changelog
 ---@toc_entry Changes in neogen plugin
-neogen.version = "2.10.0"
+neogen.version = "2.10.1"
 --minidoc_afterlines_end
 
 return neogen
--- lua/neogen/locators/typescript.lua
+++ lua/neogen/locators/typescript.lua
@@ -3,7 +3,7 @@ local default_locator = require("neogen.locators.default")
 return function(node_info, nodes_to_match)
     local found_node = default_locator(node_info, nodes_to_match)
 
-    if found_node and vim.tbl_contains({ "class_declaration", "function_declaration" }, found_node:type()) then
+    if found_node and vim.tbl_contains({ "class_declaration" }, found_node:type()) then
         local parent = found_node:parent()
         if parent and parent:type() == "export_statement" then
             return parent
