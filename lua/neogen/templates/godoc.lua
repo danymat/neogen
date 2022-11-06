1e96d6addf56cc38fcd12cbda0c5d615cde72b01
Omar Zeghouani <omarzeghouanii@gmail.com>
2022-11-06 11:32:35 +0000

    Add godoc file template

--- lua/neogen/templates/godoc.lua
+++ lua/neogen/templates/godoc.lua
@@ -2,4 +2,5 @@ return {
     { nil, " $1", { no_results = true } },
     { "func_name", " %s $1", { type = { "func" } } },
     { "type_name", " %s $1", { type = { "type" } } },
+    { "package_name", " Package %s $1", { type = { "file" } } },
 }
