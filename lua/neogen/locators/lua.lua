297795f3cd05a1bac4283e9e3f82cc9239b697ff
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2023-02-12 23:36:51 +0900

    fix: Rename ts_utils.get_node_range -> vim.treesitter.get_node_range

--- lua/neogen/locators/lua.lua
+++ lua/neogen/locators/lua.lua
@@ -4,7 +4,7 @@ local default_locator = require("neogen.locators.default")
 return function(node_info, nodes_to_match)
     -- We're dealing with a lua comment and we need to escape its grasp
     if node_info.current and node_info.current:type() == "source" then
-        local start_row, _, _, _ = ts_utils.get_node_range(node_info.current)
+        local start_row, _, _, _ = vim.treesitter.get_node_range(node_info.current)
         vim.api.nvim_win_set_cursor(0, { start_row + 1, 0 })
         node_info.current = ts_utils.get_node_at_cursor()
     end
