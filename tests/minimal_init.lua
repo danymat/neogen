b4ab597978f88b2884422f39504b5d9ff58017ac
Danymat <d.danymat@gmail.com>
2024-07-23 16:34:53 +0200

    ref: Use ensure_installed for quicker runs

--- tests/minimal_init.lua
+++ tests/minimal_init.lua
@@ -22,6 +22,11 @@ require("plenary.busted")
 vim.cmd("runtime plugin/nvim-treesitter.lua")
 
 -- Some tests require the Python parser
-vim.cmd([[TSInstallSync! python]])
+-- vim.cmd([[TSInstallSync! python]])
+require("nvim-treesitter.configs").setup({
+    ensured_installed = {
+        "python"
+    }
+})
 
 require("neogen").setup({ snippet_engine = "nvim" })
